94d00acb5c7a4f91b7147d70c413e7be4cb267e5
Mohammad Hasan <mdhasan972@gmail.com>
2021-03-07 23:14:04 +0600

    Add a ajax method

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,15 @@
-from django.http.response import HttpResponse
+from django.http import response
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def index(request):
-    return HttpResponse("<h1> Mohammad Hasan </h1>")
+    return render(request, 'firstapp/index.html', {})
+
+
+def ajax(request):
+    data = {'name':'Mohammad Hasan', 'age':20, 'mobile':'01862441950'}
+    
+    response = JsonResponse(data)
+    response['Access-Control-Allow-Origin'] = '*'
+    return response
